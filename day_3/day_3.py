0421acafc0b2d658692c8565c553e40c88985dee
Francisco <fquico1999@gmail.com>
2021-12-06 11:47:51 -0800

    Added code for part 2

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -45,8 +45,46 @@ if __name__ == "__main__":
 		epsilon_rate =  int('0b' + ''.join(epsilon_rate),2)
 
 		power_consumption = gamma_rate*epsilon_rate
-		print(power_consumption)
-		
+		#print(power_consumption)
+
+
+		### PART 2 ###
+
+		def filter(arr, idx, crit):
+			avg = np.mean(arr[:,idx])
+			if avg >= 0.5:
+				# Default round will round 0.5 to nearest even, not the desired behaviour
+				bit = 1
+			else:
+				bit = 0
+
+			if crit == 'least':
+				bit = max(abs(bit-1), 0)
+
+			arr = arr[arr[:,idx] == bit]
+			if arr.shape[0] == 1:
+				# We only have one entry - basecase - return the number
+				return arr[0]
+			elif idx == arr.shape[1]:
+				# Reached the end
+				return arr
+			else:
+				return filter(arr, idx+1, crit)
+
+		o2_gen_rating = filter(arr, 0, 'most')
+		co2_scrub_rating = filter(arr, 0, 'least')
+
+
+		# Convert integers to strings to assemble the binary digits
+		o2_gen_rating = [str(int(bit)) for bit in o2_gen_rating]
+		co2_scrub_rating = [str(int(bit)) for bit in co2_scrub_rating]
+
+		# Convert rates to decimal
+		o2_gen_rating = int('0b' + ''.join(o2_gen_rating), 2)
+		co2_scrub_rating =  int('0b' + ''.join(co2_scrub_rating),2)
+
+		life_support_rating = co2_scrub_rating*o2_gen_rating
+		print(life_support_rating)
 
 	else:
 		raise(AssertionError("$s not found in $s" % (FILENAME, pwd)))
