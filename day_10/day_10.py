9b8253982d8a63a48f08e72a0c06263744480cb1
Francisco <fquico1999@gmail.com>
2021-12-10 16:43:36 -0800

    Code for part 2 of day 10

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -23,6 +23,48 @@ def part1(data):
 					score += scores[elem]
 	return score
 
+def part2(data):
+	# We score the unmatched openings to avoid extra computation
+	score_dict = {'(':1, '[':2, '{':3, '<':4}
+	scores = []
+	closing = [')',']','}','>']
+	opening = ['(','[','{','<']
+
+	# Filter out corrupted
+	corrupted_idxs = []
+	for i,line in enumerate(data):
+		stack = []
+		for elem in list(line):
+			if elem in opening:
+				stack.append(elem)
+			elif elem in closing:
+				poped = stack.pop()
+				closingIdx = closing.index(elem)
+				openingIdx = opening.index(poped)
+				if closingIdx != openingIdx:
+					#Corrupted entry
+					corrupted_idxs.append(i)
+
+	data = [line for i, line in enumerate(data) if i not in corrupted_idxs]
+
+	for line in data:
+		score = 0
+		stack = []
+		for elem in list(line):
+			if elem in opening:
+				stack.append(elem)
+			elif elem in closing:
+				poped = stack.pop()
+		# If stack is not empty, that means that the line is incomplete
+		# Need this if statement to avoid scoring complete lines
+		if stack:
+			while stack:
+				# Get last unclosed entry
+				poped = stack.pop()
+				score = score*5 + score_dict[poped]
+			scores.append(score)
+	scores = sorted(scores)
+	return scores[len(scores)//2]
 
 
 if __name__ == "__main__":
@@ -41,8 +83,7 @@ if __name__ == "__main__":
 			data = infile.readlines()
 		data = [line.strip() for line in data]
 		
-
-		print(part1(data))
+		print(part2(data))
 
 
 	else:
