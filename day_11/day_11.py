e16bb833dfe536c805d3711fce7299658f056e8b
Francisco <fquico1999@gmail.com>
2021-12-11 22:09:06 -0800

    Added code for part 2

--- day_11/day_11.py
+++ day_11/day_11.py
@@ -44,7 +44,45 @@ def part1(grid):
 
 		print(numFlashes)
 
+def part2(grid):
+	sizeX, sizeY = grid.shape
+	ones = np.ones([sizeX, sizeY], dtype=int)
+	step = 0
+	allFlashed = False
+	while not allFlashed:
+		# Increment all by one
+		print(grid)
+		print("\n")
+		grid += ones
 
+		# Check for energy level 10 or higher
+		
+		while np.sum(grid > 9):
+			indices = np.where(grid > 9)
+			for x,y in zip(indices[0], indices[1]):
+
+				# Set to -1 to avoid double flashing
+				grid[x,y] = -1
+
+				# Increment neighbouring tiles that are not -1
+				mask = np.zeros([sizeX, sizeY], dtype=bool)
+				xmin = max(0, x-1)
+				xmax = min(sizeX+1, x+2)
+				ymin = max(0, y-1)
+				ymax = min(sizeY+1, y+2)
+				mask[xmin:xmax, ymin:ymax] = grid[xmin:xmax, ymin:ymax] != -1
+				grid[mask] += 1
+			indices = np.where(grid > 9)
+
+		if np.sum(grid-ones) == 0:
+			allFlashed = True
+		else:
+			# Set -1's to zero
+			grid[grid == -1] = 0
+			step+=1
+
+
+		print(step)
 
 
 if __name__ == "__main__":
@@ -64,7 +102,8 @@ if __name__ == "__main__":
 		data = [list(line.rstrip()) for line in data]
 		data = np.asarray(data, dtype=int)
 		
-		part1(data)
+		#part1(data)
+		part2(data)
 
 
 	else:
